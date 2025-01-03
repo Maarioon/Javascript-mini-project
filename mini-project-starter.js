06e8c4dd793129149a4c197ae82b308a1aea3803
b3c3 <b3c3@cecureintel.com>
2025-01-03 21:24:45 +0000

    Updated thje project starter js file - added functions to validate user input.

--- mini-project-starter.js
+++ mini-project-starter.js
@@ -1,21 +1,59 @@
 'use strict';
 
 /*
-* Write a program that keeps scores from Premier League football matches.
-* The input must be only limited to teams currently in the premier league this current season of 2024/25
-* The user input must be received via prompt
-* Each input must follow the format of team1 score - score team2
-* E.g. Arsenal 1 - 2 Ipswich
-* Another e.g. Man City 0 - 7 Man United
-* Any popular version of the team names are acceptable such as Man City for Manchester City.
-* Once input is received, store the match results as individuals objects within a containing array.
-* Write functions to display the most recent match results and also all match results.
-*/
+ * Write a program that keeps scores from Premier League football matches.
+ * The input must be only limited to teams currently in the premier league this current season of 2024/25
+ * The user input must be received via prompt
+ * Each input must follow the format of team1 score - score team2
+ * E.g. Arsenal 1 - 2 Ipswich
+ * Another e.g. Man City 0 - 7 Man United
+ * Any popular version of the team names are acceptable such as Man City for Manchester City.
+ * Once input is received, store the match results as individuals objects within a containing array.
+ * Write functions to display the most recent match results and also all match results.
+ */
 
-function e(i){
-    console.log(i);
+function e(i) {
+  console.log(i);
+}
+
+function quickCheckUserInput(strMatchResult){
+  let boolValidInput = false;
+
+  if (strMatchResult){
+    let arrTeamAndScore = strMatchResult.split('-');
+    e(`[INFO] Inside quickCheckUserInput | Array of Team and Score: ${arrTeamAndScore}`);
+    boolValidInput = (arrTeamAndScore.length == 2);
+  }
+
+  return boolValidInput;
+}
+
+function extractTeamNamesAndScores(strMatchResult){
+    //TODO Function
+    //Extract the team name and score into an object and also keep the result.
 }
 
 //========Main Program Starts Here==========//
-let strResultInput = prompt("Enter Match Result in the Format 'Team 1 score - score Team 2' e.g. Fulham 1 - 2 Arsenal", 'Fulham 1 - 2 Arsenal');
+let arrMatchResults = [];
+
+const strSampleResult = 'Fulham 1 - 2 Arsenal';
+
+let strResultInput = prompt(
+  `Enter Match Result in the Format 'Team 1 score - score Team 2' e.g. ${strSampleResult}`,
+  strSampleResult
+);
+
 e(strResultInput);
+
+if (quickCheckUserInput(strResultInput)) {
+  e('User Input appears to be valid.......');
+  //Extract the team scores and then validate team names
+  let x = extractTeamNamesAndScores(strResultInput);
+
+} else {
+  e(`[ERROR] User Input [${strResultInput}] is NOT a valid match result.`);
+}
+
+//Placeholder - just hold the results in arrays first
+//arrMatchResults.push(strResultInput);
+//e(arrMatchResults);
